970d33b0ba6e54b74141611e7a6ff758be76f522
MagdalenaJT <caspermklo@gmail.com>
2015-06-21 23:46:18 +0200

    Code

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
+## Function creates a matrix that could cash it inverse
+
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(x) {
+    meatrix <<- x;
+    inverse <<- NULL;
+  }
+  get <- function() return(meatrix);
+  setinv <- function(inv) inverse <<- inv;
+  getinv <- function() return(inverse);
+  return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+## Function computes the inverse of matrix. If the matrix has been calculated then it could retrive inverse form cache.
+
+cacheSolve <- function(meatrix, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  inverse <- meatrix$getinv()
+  if(!is.null(inverse)) {
+    message("Getting from cache")
+    return(inverse)
+  }
+  data <- meatrix$get()
+  inverse <- solve(data, ...)
+  meatrix$setinv(inverse)
+  return(inverse)
+  }
